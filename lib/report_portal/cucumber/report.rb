3f6aad1219f3cf576ecf4c8227cdd05b14071ce0
youngkih <youngkihong92@gmail.com>
2018-11-05 13:49:29 -0800

    logging for checking index

--- lib/report_portal/cucumber/report.rb
+++ lib/report_portal/cucumber/report.rb
@@ -177,7 +177,9 @@ module ReportPortal
         path_components.each_with_index do |path_component, index|
           child_node = parent_node[path_component]
           unless child_node # if child node was not created yet
-            if index < path_components.size - 1
+            if index < 2
+              puts "Index is less than 2!!"
+            elsif index < path_components.size - 1
               name = "Folder: #{path_component}"
               description = nil
               tags = []
